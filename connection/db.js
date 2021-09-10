14e49c800c77b7a30dc42e13deec1ca3958913af
Ashish Kotyan <Ashishkotyan20@gmail.com>
2021-09-10 13:18:01 +0530

    Finalize-Backend

--- connection/db.js
+++ connection/db.js
@@ -3,7 +3,7 @@ import  mongoose  from "mongoose";
 const Connection = async(username, password) => {
     try{
 
-    const URL = ``mongodb://${username}:${password}@inshort-data-shard-00-00.rl3b5.mongodb.net:27017,inshort-data-shard-00-01.rl3b5.mongodb.net:27017,inshort-data-shard-00-02.rl3b5.mongodb.net:27017/INSHORTS?ssl=true&replicaSet=atlas-ll6fro-shard-0&authSource=admin&retryWrites=true&w=majority`
+    const URL = `mongodb://${username}:${password}@inshort-data-shard-00-00.rl3b5.mongodb.net:27017,inshort-data-shard-00-01.rl3b5.mongodb.net:27017,inshort-data-shard-00-02.rl3b5.mongodb.net:27017/INSHORTS?ssl=true&replicaSet=atlas-ll6fro-shard-0&authSource=admin&retryWrites=true&w=majority`
    await mongoose.connect(URL,{useNewUrlParser:true})
 
         console.log("Database is connected");
